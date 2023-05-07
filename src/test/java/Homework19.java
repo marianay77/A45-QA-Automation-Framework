e026c52884ca20653408e636d0f57237ab771fc4
marianay77 <130865691+marianay77@users.noreply.github.com>
2023-05-07 13:38:38 -0400

    updated with the GHERKIN style method

--- src/test/java/Homework19.java
+++ src/test/java/Homework19.java
@@ -6,9 +6,11 @@ import org.testng.annotations.Parameters;
 public class Homework19 extends BaseTest {
     @Test()
     public void deletePlaylist () throws InterruptedException {
+        //GIVEN
         login ("demo@class.com", "te$t$tudent");
         Thread.sleep (2000);
     }
+    //WHEN
     public void openPlaylist () throws InterruptedException {
         WebElement openPlaylist = driver.findElement (By.xpath ("//section[@id='playlists'] //a[contains(text(), 'Mara2023')]"));
         openPlaylist.click ();
@@ -19,6 +21,7 @@ public class Homework19 extends BaseTest {
             deletePlaylistButton.click ();
             Thread.sleep (2000);
         }
+        //THEN
             public void notifMsg ()throws InterruptedException {
             WebElement notifMsg = driver.findElement (By.cssSelector ("div.success.show"));
             Assert.assertTrue (notifMsg.getText ().contains ("Deleted playlist 'Mara2023'."));
